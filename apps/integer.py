c9c8a975a92d24784f5758d183617ce4dfd6c08a
YZeit <84660151+YZeit@users.noreply.github.com>
2021-05-24 15:08:57 +0100

    Update integer.py

--- apps/integer.py
+++ apps/integer.py
@@ -289,7 +289,7 @@ def app():
         st.pyplot(fig2)
     with c2:
         st.header('The projected feasible region')
-        fig2, ax1, = plt.figure()
+        fig2 = plt.figure()
         plt.xlabel("$z_1$")
         plt.ylabel("$z_2$")
         plt.yticks([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
@@ -297,7 +297,7 @@ def app():
         plt.xticks([-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                     14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25])
         plt.grid(True)
-        #ax1 = fig2.add_subplot()
+        ax1 = fig2.add_subplot()
         ax1.set_xlim([-10, 25])
         ax1.set_ylim([-10, 25])
         ax1.tick_params(axis='both', which='major', labelsize=5)
