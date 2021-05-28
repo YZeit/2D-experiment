57360c942491631b3059118654177a703e80784d
YZeit <84660151+YZeit@users.noreply.github.com>
2021-05-28 13:53:24 +0100

    Update 2D-experiment.py

--- 2D-experiment.py
+++ 2D-experiment.py
@@ -1,14 +1,15 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import linear, integer# import your app modules here
+from apps import linear, integer, integer_salvo# import your app modules here
 
 st.set_page_config(layout="wide")
 app = MultiApp()
 
 
 # Add all your application here
-app.add_app("Mixed-Integer Linear programming model (MILP)", linear.app)
-app.add_app("Integer Linear programming model (ILP)", integer.app)
+app.add_app("Linear model", linear.app)
+app.add_app("Integer model (Yannik)", integer.app)
+app.add_app("Integer model (Salvo)", integer_salvo.app)
 
 # The main app
 app.run()
