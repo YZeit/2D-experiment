1c2fa8d255b8593f8f7e1c93f3278b1043d0aa3f
YZeit <84660151+YZeit@users.noreply.github.com>
2021-06-02 18:45:54 +0100

    Update 2D-experiment.py

--- 2D-experiment.py
+++ 2D-experiment.py
@@ -1,15 +1,16 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import linear, integer, integer_salvo# import your app modules here
+from apps import linear, integer, integer_salvo, test# import your app modules here
 
 st.set_page_config(layout="wide")
 app = MultiApp()
 
 
 # Add all your application here
-#app.add_app("Linear model", linear.app)
+app.add_app("Linear model", linear.app)
 app.add_app("Integer model (Yannik)", integer.app)
 app.add_app("Integer model (Salvo)", integer_salvo.app)
+app.add_app("Test Choquet metric", test.app)
 
 # The main app
 app.run()
