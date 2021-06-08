7e03e576a188aafdb60b48e2d8c8c613704d428e
YZeit <84660151+YZeit@users.noreply.github.com>
2021-06-08 15:53:29 +0100

    Update 2D-experiment.py

--- 2D-experiment.py
+++ 2D-experiment.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import linear, integer, integer_salvo, test, test2 # import your app modules here
+from apps import linear, integer, integer_salvo, test, test2, decomposed_choquet_L1, decomposed_choquet_Linf # import your app modules here
 
 st.set_page_config(layout="wide")
 app = MultiApp()
@@ -10,8 +10,8 @@ app = MultiApp()
 #app.add_app("Linear model", linear.app)
 #app.add_app("Integer model (Yannik)", integer.app)
 #app.add_app("Integer model (Salvo)", integer_salvo.app)
-app.add_app("Test Choquet metric", test.app)
-app.add_app("Test Choquet metric (2)", test2.app)
+app.add_app("Choquet min-norm", decomposed_choquet_L1.app)
+app.add_app("Choquet Tschebycheff (max-)norm", decomposed_choquet_Linf.app)
 
 # The main app
 app.run()
