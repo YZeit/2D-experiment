417e50e0b09a8f5f87c44afde24aa566a203207a
YZeit <84660151+YZeit@users.noreply.github.com>
2022-05-16 23:19:15 +0100

    Update additive_approach_Ruiz1.py

--- apps/additive_approach_Ruiz1.py
+++ apps/additive_approach_Ruiz1.py
@@ -91,15 +91,17 @@ def app():
     st.subheader('The Choquet program')
     st.write('To find the feasible solution $x$ that is closest to the reference point with respect to the introduced Choquet distance, we look for a feasible solution $x$ minimizing $C_\mu (\Delta (x))$. The following optimization program can be defined as the Choquet program:')
     st.latex(r'''\min_{x \in X} C_\mu (\Delta(x))''')
-    st.write('Through replacing $C_\mu \left(\Delta (x) \right)$ by the Choquet metric with respect to the 2-additive fuzzy measure, the Choquet program can be written as:')
+    st.write('Through replacing $C_\mu\left(\Delta(x)\right)$ by the Choquet metric with respect to the 2-additive fuzzy measure, the Choquet program can be written as:')
     st.latex(r'''\min_{x \in X} \left\{ \sum_{\{i\}\subseteq G} a(\{i\})\Delta_i (x) +\sum_{\{i,j\}\subseteq G} a(\{i,j\})\min\left\{\Delta_i (x), \Delta_j (x)\right\} \right\}''')
     st.write(' In order to linearize the $\min$ formulation in the previous equation, we introduce a new non-negative variable $\beta$. The linearized Choquet program can then be formulated as:')
-    st-latex(r'''         \min               &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \Delta_i (x) + \sum_{\{i,j\}\subseteq G} a(\{i,j\}) \beta(\{i,j\})}  \\
+    st-latex(r'''\begin{array}{rl}
+         \min               &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \Delta_i (x) + \sum_{\{i,j\}\subseteq G} a(\{i,j\}) \beta(\{i,j\})}  \\
          \mbox{subject to:} & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
          & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_j (x) - My_{(\{i,j\}),2}}, \;\; \forall (\{i,j\}) \subseteq G\\
          & {\displaystyle y_{(\{i,j\}),1} + y_{(\{i,j\}),2} = 1}, \;\; \forall (\{i,j\}) \subseteq G \\
                             & x \in X \\ 
-                            & \beta(\{i,j\}) \geqslant 0,\;\; \forall (\{i,j\}) \subseteq G''')
+                            & \beta(\{i,j\}) \geqslant 0,\;\; \forall (\{i,j\}) \subseteq G . 
+    \end{array}''')
     st.write('with $M$ being an arbitrary big value.')
     
     st.subheader('Preference Assumptions:')
