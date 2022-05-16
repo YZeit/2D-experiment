5657729c01f10d10d952324af3a85b654329f5c0
YZeit <84660151+YZeit@users.noreply.github.com>
2022-05-16 23:32:24 +0100

    Update additive_approach_Ruiz1.py

--- apps/additive_approach_Ruiz1.py
+++ apps/additive_approach_Ruiz1.py
@@ -93,17 +93,62 @@ def app():
     st.latex(r'''\min_{x \in X} C_\mu (\Delta(x))''')
     st.write('Through replacing $C_\mu(\Delta(x))$ by the Choquet metric with respect to the 2-additive fuzzy measure, the Choquet program can be written as:')
     st.latex(r'''\min_{x \in X} \left\{ \sum_{\{i\}\subseteq G} a(\{i\})\Delta_i (x) +\sum_{\{i,j\}\subseteq G} a(\{i,j\})\min\left\{\Delta_i (x), \Delta_j (x)\right\} \right\}''')
-    st.write(' In order to linearize the $\min$ formulation in the previous equation, we introduce a new non-negative variable $\beta$. The linearized Choquet program can then be formulated as:')
+    st.write(' In order to linearize the $\min$ formulation in the previous equation, we introduce a new non-negative variable $ \beta $. The linearized Choquet program can then be formulated as:')
     st.latex(r'''\begin{array}{rl}
          \min               &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \Delta_i (x) + \sum_{\{i,j\}\subseteq G} a(\{i,j\}) \beta(\{i,j\})}  \\
-         \mbox{subject to:} & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
+         subject to: & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
          & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_j (x) - My_{(\{i,j\}),2}}, \;\; \forall (\{i,j\}) \subseteq G\\
          & {\displaystyle y_{(\{i,j\}),1} + y_{(\{i,j\}),2} = 1}, \;\; \forall (\{i,j\}) \subseteq G \\
                             & x \in X \\ 
                             & \beta(\{i,j\}) \geqslant 0,\;\; \forall (\{i,j\}) \subseteq G . 
     \end{array}''')
     st.write('with $M$ being an arbitrary big value.')
-    
+    st.write('Using the ideal outcome vector as reference point ensures to find a supported efficient solution. However, the proposed Choquet metric in its current setting has two main drawbacks that result in the program's inability to find all non-dominated solutions:')
+    st.write('1. negative distances to the reference point can not be considered')
+    st.write('2. solutions that dominate the reference point can not be identified')
+    st.write('Among these issues, the Choquet metric is limited in reaching unsupported non-dominated solutions as it is also the case for the simple additive metric (Ruiz et al, 2008). To overcome these issues, we will present several adjustments to the initially proposed Choquet program.')         
+    st.write('The first problem results from the Choquet integral's requirements that do not allow aggregating negative values. Given the example shown in overleaf one could argue that solution C should be preferable over solution A, since it significantly improves the $z_2$ component while the distance to the $z_1$ component is smaller than the $z_2$ distance of solution A to the reference point. However, the Choquet program is not able to observe solution C with the selected reference point.')
+    st.write('Based on the approach to avoid penalizing negative deviations for an additive achievement scalarizing function shown in (Ruiz et al, 2008), a modification of the Choquet program that only allows positive deviations to be considered is introduced:')
+    st.latex(r'''    \begin{array}{rl}
+         \min &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \max \{\Delta_i (x), 0\} + \sum_{\{i,j\}\subseteq G} a(\{i,j\}) \max \{\beta(\{i,j\}), 0\}}  \\
+         subject to: & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
+         & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_j (x) - My_{(\{i,j\}),2}}, \;\; \forall (\{i,j\}) \subseteq G\\
+         & {\displaystyle y_{(\{i,j\}),1} + y_{(\{i,j\}),2} = 1}, \;\; \forall (\{i,j\}) \subseteq G \\
+        & x \in X.
+    \end{array}''')
+    st.write('To make the modified program differentiable, the model shown above is linearized, introducing new binary variable $v$ and $w$:')
+    st.latex(r'''  \begin{array}{rl}
+         \min &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \Delta_i (x) v_i + \sum_{\{i,j\}\subseteq G} a(\{i,j\})  \beta(\{i,j\}) w_{i,j}}  \\
+         subject to: & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
+        & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_j (x) - My_{(\{i,j\}),2}}, \;\; \forall (\{i,j\}) \subseteq G\\
+        & {\displaystyle y_{(\{i,j\}),1} + y_{(\{i,j\}),2} = 1}, \;\; \forall (\{i,j\}) \subseteq G \\
+        & \Delta_i(x) \leqslant Mv_i, \forall (\{i\}) \subseteq G \\
+        & \Delta_i(x) \geqslant M(v_i-1), \forall (\{i\}) \subseteq G \\
+        & \beta(\{i,j\}) \leqslant Mw_{i,j}, \forall (\{i,j\}) \subseteq G \\
+        & \beta(\{i,j\}) \geqslant M(w_{i,j}-1), \forall (\{i,j\}) \subseteq G \\
+        & v_i \in {0,1}, \forall (\{i\}) \subseteq G \\
+        & w_{i,j} \in {0,1}, \forall (\{i,j\}) \subseteq G \\
+        & x \in X. 
+    \end{array}   ''')
+    st.write('As it can be seen in overleaf the modified Choquet program is able to reach solution C which is preferable over solution A due to the aforementioned arguments. Negative deviations are no longer penalized, through which the program can identify solutions with negative distance components to the reference point whenever there exists at least one distance component having a positive value.')         
+    st.write('However, if there is a solution that dominates the reference point, which means that all distance components are negative, the proposed Choquet program is not able to find that solution. As the same issue already appeared for the simple additive $L_1$-norm, Ruiz et al (2008) proposed a procedure in which another program first verifies whether there exists a solution that dominates the selected reference point or not. In case there exists a solution that dominates the reference point, Ruiz et al (2008) proposed to simply minimize the weighted additive distance of the problem at hand. Since we want to additionally consider interaction between criteria, further adjustments are required. A possible way to find non-dominated solutions whenever a reference point is dominated by feasible solution(s) is to replace the selected reference point with the ideal outcome vector $z^*$. The distance components are then defined as:')
+    st.latex(r'''        \Delta_k(x) = \frac{z_k^{*} - z_k(x)}{z_k^{*} - z_k^{nad}}.''')
+    st.write('Adding a constraint to the Choquet program that only allows $z_i(x)$ to be lower or equal to the corresponding reference point component subsequently allows identifying all solutions that dominates the originally selected reference point:')         
+    st.latex(r'''\begin{array}{rl}
+         \min &  {\displaystyle \sum_{\{i\}\subseteq G} a(\{i\}) \Delta_i (x) v_i + \sum_{\{i,j\}\subseteq G} a(\{i,j\})  \beta(\{i,j\}) w_{i,j}}  \\
+         subject to: & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_i (x) - My_{(\{i,j\}),1}}, \;\; \forall (\{i,j\}) \subseteq G\\
+        & {\displaystyle \beta(\{i,j\}) \geqslant \Delta_j (x) - My_{(\{i,j\}),2}}, \;\; \forall (\{i,j\}) \subseteq G\\
+        & {\displaystyle y_{(\{i,j\}),1} + y_{(\{i,j\}),2} = 1}, \;\; \forall (\{i,j\}) \subseteq G \\
+        & \Delta_i(x) \leqslant Mv_i, \forall (\{i\}) \subseteq G \\
+        & \Delta_i(x) \geqslant M(v_i-1), \forall (\{i\}) \subseteq G \\
+        & \beta(\{i,j\}) \leqslant Mw_{i,j}, \forall (\{i,j\}) \subseteq G \\
+        & \beta(\{i,j\}) \geqslant M(w_{i,j}-1), \forall (\{i,j\}) \subseteq G \\
+        & v_i \in {0,1}, \forall (\{i\}) \subseteq G \\
+        & w_{i,j} \in {0,1}, \forall (\{i,j\}) \subseteq G \\
+        & z_i(x) \leqslant z^r_i, \forall (\{i\}) \subseteq G \\
+        & x \in X.
+    \end{array}''')
+    st.write('If there is no feasible solution to that program, the originally selected reference point can be stated as a non-dominated point in the objective space. In that case, the Choquet program is solved as proposed before, using the reference point selected by the DM.')         
     st.subheader('Preference Assumptions:')
     st.write('Please select the preference assumptions in the sidebar')
     st.sidebar.header('Preference Information')
